5a7b588d324aaba4fc69e961d4b2a5b20a34001d
Eric Smalley <eric@smalley.nl>
2023-03-23 15:27:26 +1300

    Add an environment variable so that we can test that aspect during continuous integration as well

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Header from "./components/Header";
 function App() {
   return (
     <div className="App">
-      <Header text="Hello world!" />
+      <Header text={ `Hello ${process.env.REACT_APP_HELLO_NAME}!` } />
     </div>
   );
 }
