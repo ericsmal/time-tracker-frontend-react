7937f49d008616edc2a6c5e2d145ca9fe7a5a56d
Eric Smalley <eric@smalley.nl>
2023-03-23 17:05:18 +1300

    Adjust App test to accommodate for environment variable in header

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,7 +5,7 @@ import App from './App';
 describe('App component', () => {
   it('contains an h1 header on the first page', () => {
     render(<App />);
-    const h1Element = screen.getByText(/hello world/i);
+    const h1Element = screen.getByText(/hello/i);
     expect(h1Element).toBeInTheDocument();
     expect(h1Element.tagName).toStrictEqual('H1');
   });
